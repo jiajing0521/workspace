dee2a18060cdec4e93935242c88a08f361f6c5fb
dllo <757959688@qq.com>
2016-12-05 20:56:23 +0800

    school

---
+++ WEB/99_模板制作/9999.练习/markdown/reactNative.md
@@ -0,0 +1,11 @@
+#reactNative环境安装
+		brew install watchman
+		brew install flow
+		npm install -g react-native-cli
+		
+		//新建一个rn项目
+		cd 文件夹
+		react-native init 名字
+		
+		//如果以上安装时间过于长,可以分段安装
+		brew install yarn
--- WEB/99_模板制作/9999.练习/zrender/demo/demo.js
+++ WEB/99_模板制作/9999.练习/zrender/demo/demo.js
@@ -1,52 +1,89 @@
 /**
  * Created by dllo on 16/12/2.
  */
-var zrender = require("");
-var zr = zrender.init(document.getElementById('main'));
-zr.clear();
-var color = require('src/tool/color');
-var colorIdx = 0;
-var width = Math.ceil(zr.getWidth());
-var height = Math.ceil(zr.getHeight());
 
-// 圆形
-zr.addShape({
-    shape : 'circle',
-    id : zr.newShapeId('circle'),
-    style : {
-        x : 100,
-        y : 100,
-        r : 50,
-        brushType : 'both',
-        color : 'rgba(220, 20, 60, 0.8)',          // rgba supported
-        strokeColor : color.getColor(colorIdx++),  // getColor from default palette
-        lineWidth : 5,
-        text :'circle',
-        textPosition :'inside'
-    },
-    hoverable : true,   // default true
-    draggable : true,   // default false
-    clickable : true,   // default false
+    require.config({
+        packages: [
+            {
+                name: 'zrender',
+                location: 'zrender/src',
+                main: 'zrender'
+            }
+        ]
+    });
 
-    // 可自带任何有效自定义属性
-    _name : 'Hello~',
-    onclick: function(params){
-        alert(params.target._name);
-    },
+    require(["zrender", 'zrender/graphic/Text'], function(zrender, ZText){
+        // 初始化zrender
+        var zr = zrender.init(document.getElementById("main"), {
+            renderer: 'svg'
+        });
 
-    // 响应事件并动态修改图形元素
-    onmousewheel: function(params){
-        var eventTool = require('zrender/tool/event');
-        var delta = eventTool.getDelta(params.event);
-        var r = params.target.style.r;
-        r += (delta > 0 ? 1 : -1) * 10;
-        if (r < 10) {
-            r = 10;
-        };
-        zr.modShape(params.target.id, {style: {r: r}});
-        zr.refresh();
-        eventTool.stop(params.event);
-    }
-});
+        var text = new ZText({
+            // position : [50, 50],
+            // rotation: 1,
+            // scale: [2, 2],
+            style: {
+                x: 0,
+                y: 0,
+                text: '国国国国\n国国国国\n国国国国',
+                width: 50,
+                height: 50,
+                fill: '#c0f',
+                textFont: '18px Microsoft Yahei',
+                textBaseline: 'top'
+            },
+            draggable: true
+        });
+        zr.add(text);
+    });
 
-zr.render();
+
+//var zrender = require("");
+//var zr = zrender.init(document.getElementById('main'));
+//zr.clear();
+//var color = require('src/tool/color');
+//var colorIdx = 0;
+//var width = Math.ceil(zr.getWidth());
+//var height = Math.ceil(zr.getHeight());
+//
+//// 圆形
+//zr.addShape({
+//  shape : 'circle',
+//  id : zr.newShapeId('circle'),
+//  style : {
+//      x : 100,
+//      y : 100,
+//      r : 50,
+//      brushType : 'both',
+//      color : 'rgba(220, 20, 60, 0.8)',          // rgba supported
+//      strokeColor : color.getColor(colorIdx++),  // getColor from default palette
+//      lineWidth : 5,
+//      text :'circle',
+//      textPosition :'inside'
+//  },
+//  hoverable : true,   // default true
+//  draggable : true,   // default false
+//  clickable : true,   // default false
+//
+//  // 可自带任何有效自定义属性
+//  _name : 'Hello~',
+//  onclick: function(params){
+//      alert(params.target._name);
+//  },
+//
+//  // 响应事件并动态修改图形元素
+//  onmousewheel: function(params){
+//      var eventTool = require('zrender/tool/event');
+//      var delta = eventTool.getDelta(params.event);
+//      var r = params.target.style.r;
+//      r += (delta > 0 ? 1 : -1) * 10;
+//      if (r < 10) {
+//          r = 10;
+//      };
+//      zr.modShape(params.target.id, {style: {r: r}});
+//      zr.refresh();
+//      eventTool.stop(params.event);
+//  }
+//});
+//
+//zr.render();
