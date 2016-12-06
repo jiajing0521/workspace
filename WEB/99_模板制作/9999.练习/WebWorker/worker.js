d69f2be3363bea7ba0e83183c7ffbdd59411fb67
dllo <757959688@qq.com>
2016-12-06 19:04:53 +0800

    school

---
+++ WEB/99_模板制作/9999.练习/WebWorker/worker.js
@@ -0,0 +1,15 @@
+function timeCount(){
+	for (var i = 0, sum = 0; i < 100; i++) {
+		for (var j = 0; j < 100000000; j++) {
+			sum = sum+j;
+		}
+	}
+	postMessage(sum);
+}
+
+postMessage( "1.."+new Date());
+timeCount();
+postMessage( "2.."+new Date());
+
+
+
